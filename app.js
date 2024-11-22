7356aa265b433c88c335e5f00021d3eddbaa142a
ArnabMallick <arnavmallick12006@gmail.com>
2024-11-23 01:27:30 +0530

    Improved home page and fixed some bugs

--- app.js
+++ app.js
@@ -1,5 +1,30 @@
+// <!-- dropdown of diu and Convocation -->
+
+        // Toggle dropdown visibility on button click
+        document.querySelector(".dropbtn").addEventListener("click", function() {
+            const dropdownContent = document.querySelector(".dropdown-content");
+            dropdownContent.classList.toggle("show");
+            dropdownContent.classList.add("active");
+        });
+
+        // Close the dropdown if clicked outside
+        window.onclick = function(event) {
+            if (!event.target.matches('.dropbtn')) {
+                const dropdowns = document.getElementsByClassName("dropdown-content");
+                for (let i = 0; i < dropdowns.length; i++) {
+                    const openDropdown = dropdowns[i];
+                    if (openDropdown.classList.contains('show')) {
+                        openDropdown.classList.remove('show');
+                    }
+                }
+            }
+        }
+
+
+
+
 /*=============== SWIPER JS ===============*/
-let swiperCards = new Swiper(".card__content", {
+let swiperCards_ln = new Swiper(".card__content_ln", {
     loop: true,
     spaceBetween: 32,
     grabCursor: true,
@@ -11,8 +36,64 @@ let swiperCards = new Swiper(".card__content", {
     },
   
     navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
+      nextEl: ".ln-next",
+      prevEl: ".ln-prev",
+    },
+  
+    breakpoints:{
+      600: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+  
+/*=============== SWIPER JS ===============*/
+let swiperCards_ec = new Swiper(".card__content_ec", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".ec-next",
+      prevEl: ".ec-prev",
+    },
+  
+    breakpoints:{
+      600: {
+        slidesPerView: 2,
+      },
+      968: {
+        slidesPerView: 3,
+      },
+    },
+  });
+  
+  
+/*=============== SWIPER JS ===============*/
+let swiperCards_rh = new Swiper(".card__content_rh", {
+    loop: true,
+    spaceBetween: 32,
+    grabCursor: true,
+  
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+      dynamicBullets: true,
+    },
+  
+    navigation: {
+      nextEl: ".rh-next",
+      prevEl: ".rh-prev",
     },
   
     breakpoints:{
